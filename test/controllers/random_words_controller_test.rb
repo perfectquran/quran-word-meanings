cceb4a67f94f1b8df3a0ed4b29c941177009a8d9
Abdullah Hashim <abdul@hey.com>
2021-06-20 01:23:08 -0400

    Require authenticated users

--- test/controllers/random_words_controller_test.rb
+++ test/controllers/random_words_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class RandomWordsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    sign_in users(:abdullah)
+  end
+
   test "get show" do
     get root_url
     assert_response :success
